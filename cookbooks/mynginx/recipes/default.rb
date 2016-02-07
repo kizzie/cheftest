c267152e86e1c4c3c53693f608403671db37aead
Kat <kat.samperi@gmail.com>
2016-02-07 16:19:28 +0000

    Updated cookbook to have the correct settings

--- cookbooks/mynginx/recipes/default.rb
+++ cookbooks/mynginx/recipes/default.rb
@@ -7,6 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
 package 'install nginx' do
   package_name 'nginx'
+  subscribes :restart, 'cookbook_file[/etc/nginx/sites-available/default]', :immediately
+end
+
+cookbook_file '/etc/nginx/sites-available/default' do
+  owner 'root'
+  group 'root'
+  source 'default'
+  mode '0644'
+  action :create
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  owner 'root'
+  group 'root'
+  source 'nginx.conf'
+  action :create
 end
