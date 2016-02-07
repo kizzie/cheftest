d89fc15c366a2e53792d1c58c91eb7ddd54b1bb2
Kat <kat.samperi@gmail.com>
2016-02-07 16:19:44 +0000

    app servers added

--- servers.rb
+++ servers.rb
@@ -1,19 +1,19 @@
-#require 'chef/provisioning'
-
 require 'chef/provisioning/vagrant_driver'
+
 with_driver 'vagrant'
 
-vagrant_box 'webserver' do
-  url 'http://files.vagrantup.com/precise64.box'
+  #nginx machine
+  vagrant_box 'webserver' do
+    url 'http://files.vagrantup.com/precise64.box'
+  end
+
+  # app machine
+  # vagrant_box 'app1' do
+  #  url 'http://files.vagrantup.com/precise64.box'
+  # end
+
+1.upto(2) do |i|
+  vagrant_box 'app#{i}' do
+    url 'http://files.vagrantup.com/precise64.box'
+  end
 end
-
-#1.upto(2) do |i|
-#  vagrant_box 'app#{i}' do
-#    url 'http://files.vagrantup.com/precise64.box'
-#  end
-#end
-
-with_machine_options :vagrant_options => {
-  'vm.box' => 'ubuntu/trusty64',
-  'vm.network' => ':forwarded_port, guest: 80, host: 8080'
-}
