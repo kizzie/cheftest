ef1b4ea57d89e9e3266908296aff67af57d9a279
Kat <kat.samperi@gmail.com>
2016-02-07 16:20:06 +0000

    provisioning one server

--- provision.rb
+++ provision.rb
@@ -1,11 +1,35 @@
 machine_batch do
+
+##nginx machine setup
+  webserveroptions = {
+    vagrant_options: {
+      'vm.box' => 'ubuntu/trusty64',
+      'vm.network' => ':forwarded_port, guest: 80, host: 8080'
+    },
+  }
+
   machine 'webserver' do
     recipe 'mynginx'
+    machine_options webserveroptions
   end
-  # Create 2 application machines
+
+## app server machine setup
+  appoptions = {
+     vagrant_options: {
+       'vm.box' => 'ubuntu/trusty64',
+       'vm.network' => ':forwarded_port, guest: 8484, host: 8484'
+     },
+   }
+
+   machine 'app1' do
+     recipe 'app'
+     machine_options appoptions
+   end
+
+
   #1.upto(2) do |i|
   #  machine "app#{i}" do
-  #    recipe '???'
+  #    recipe 'app'
   #  end
   #end
 end
