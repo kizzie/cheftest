5ca91bd8f8505cb633dcab1d902615e3b8551e56
Kat <kat.samperi@gmail.com>
2016-02-07 16:21:07 +0000

    startup.sh script to bring all online

--- startup.sh
+++ startup.sh
@@ -5,6 +5,16 @@ gem install chef-provisioning chef-provisioning-vagrant
 export CHEF_DRIVER=vagrant
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
 
+#build the code -- this isn't very elegent!
+#go build src/app.go
+#mv app cookbooks/app/files/
+
 #bring the boxes up
 #configure boxes
+#may need sudo - test this?
 chef-client -z servers.rb provision.rb
+
+#say how to get to the machine
+echo '*******************************************************'
+echo 'To see the app point a browser at http://localhost:8080'
+echo '*******************************************************'
